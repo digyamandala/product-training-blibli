80fe126b43f22fe8a0e987b69908610807d006bb
Digya Mandala Putra <digyamp@gmail.com>
2019-03-04 17:31:10 +0700

    fixing product.java

--- src/main/java/com/company/productapp/Product.java
+++ src/main/java/com/company/productapp/Product.java
@@ -2,57 +2,37 @@ package com.company.productapp;
 
 public class Product {
 
-    private int userid;
-    private String name;
-    private String email;
-    private String password;
-    private String address;
+    private int productid;
+    private String productName;
+    private String category;
 
-    public Product(int userid, String name, String email, String password, String address) {
-        this.userid = userid;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.address = address;
+    public Product(int productid, String productName, String category) {
+        this.productid = productid;
+        this.productName = productName;
+        this.category = category;
     }
 
-    public int getUserid() {
-        return userid;
+
+    public String getCategory() {
+        return category;
     }
 
-    public void setUserid(int userid) {
-        this.userid = userid;
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public int getProductid() {
+        return productid;
     }
 
-    public String getName() {
-        return name;
+    public void setProductid(int productid) {
+        this.productid = productid;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getProductName() {
+        return productName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 }
